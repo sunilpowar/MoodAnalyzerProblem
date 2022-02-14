d755aa35908b1fa4ffd9a46cf6481ff97fa28e99
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-14 23:21:32 +0530

    UC1

---
+++ MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        public string Mood(string message)
+        {
+            if (message.ToLower().Contains("Happy"))
+            {
+                return "Happy";
+            }
+            else
+            {
+                return "Sad";
+            }
+        }
+    }
+}
